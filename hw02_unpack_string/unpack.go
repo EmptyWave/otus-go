e4aa475be7ec5d47a214743f9199849b32b16caa
Дмитрий Юденков <dmitriy.yudenkov@pochtavip.com>
2022-12-15 12:55:25 +0300

    hw02_unpack_string add hw version 2

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,65 +9,38 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(str string) (string, error) {
-	var shield bool
-	var prevVal rune
-	var builder, tempBuilder strings.Builder
+	var builder strings.Builder
+	var i int
 
-	for i, curVal := range str {
-		if unicode.IsDigit(curVal) && i == 0 {
+	shieldedBackSlash := '\\'
+
+	explodedStr := []rune(str)
+	explodedStrLength := len(explodedStr)
+
+	for i < explodedStrLength {
+		curVal := explodedStr[i]
+
+		if unicode.IsDigit(curVal) {
 			return "", ErrInvalidString
 		}
 
-		if unicode.IsLetter(curVal) {
-			if shield {
-				shield = false
+		if curVal == shieldedBackSlash && i+1 < explodedStrLength {
+			i++
+			curVal = explodedStr[i]
 
-				curVal = prevVal + curVal
-			} else if unicode.IsLetter(prevVal) {
-				builder.WriteString(tempBuilder.String())
-				tempBuilder.Reset()
-			}
-
-			tempBuilder.WriteRune(curVal)
-			prevVal = curVal
-		} else if unicode.IsDigit(curVal) {
-			if shield {
-				shield = false
-
-				if unicode.IsLetter(prevVal) {
-					builder.WriteString(tempBuilder.String())
-					tempBuilder.Reset()
-				}
-
-				tempBuilder.WriteRune(curVal)
-				prevVal = curVal
-			} else {
-				if tempBuilder.Len() == 0 {
-					return "", ErrInvalidString
-				}
-
-				builder.WriteString(strings.Repeat(tempBuilder.String(), int(curVal-'0')))
-				tempBuilder.Reset()
-			}
-		} else if curVal == '\\' {
-			if tempBuilder.Len() > 0 {
-				builder.WriteString(tempBuilder.String())
-				tempBuilder.Reset()
-			}
-
-			if prevVal == '\\' {
-				shield = false
-				tempBuilder.WriteRune(curVal)
-				prevVal = rune(0)
-			} else {
-				shield = true
-				prevVal = curVal
+			if curVal != shieldedBackSlash && !unicode.IsDigit(explodedStr[i]) {
+				return "", ErrInvalidString
 			}
 		}
-	}
 
-	if tempBuilder.Len() > 0 {
-		builder.WriteString(tempBuilder.String())
+		if i+1 < explodedStrLength && unicode.IsDigit(explodedStr[i+1]) {
+			i++
+			builder.WriteString(strings.Repeat(string(curVal), int(explodedStr[i]-'0')))
+		} else {
+			builder.WriteRune(curVal)
+		}
+
+		i++
 	}
 
 	return builder.String(), nil
