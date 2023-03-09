29690c40b24eb052c847e7bdb156b27ef0e88b30
Дмитрий Юденков <dmitriy.yudenkov@pochtavip.com>
2023-03-09 11:44:59 +0300

    hw07_file_copying add hw

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb"
 )
 
 var (
@@ -10,6 +15,65 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if len(fromPath) == 0 {
+		return fmt.Errorf("%w. Wrong key 'from': %q", ErrUnsupportedFile, fromPath)
+	}
+	if len(toPath) == 0 {
+		return fmt.Errorf("%w. Wrong key 'to': %q", ErrUnsupportedFile, toPath)
+	}
+
+	from, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer from.Close()
+
+	info, err := from.Stat()
+	if err != nil {
+		return err
+	}
+	sizeFile := info.Size()
+
+	if sizeFile == 0 {
+		return fmt.Errorf("wrong key 'from': %q, unknown file length. %w ", fromPath, ErrUnsupportedFile)
+	}
+	if offset > sizeFile {
+		return fmt.Errorf("wrong key 'offset' = %d, file size = %d. %w ", offset, sizeFile, ErrOffsetExceedsFileSize)
+	}
+	if offset > 0 {
+		_, err = from.Seek(offset, 0)
+		if err != nil {
+			return err
+		}
+	}
+	if limit == 0 {
+		limit = sizeFile
+	}
+
+	replica, err := os.OpenFile(toPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o644)
+	if err != nil {
+		return err
+	}
+	defer replica.Close()
+
+	realLimit := realLimit(offset, limit, sizeFile)
+
+	bar := pb.New64(realLimit)
+	barReader := bar.NewProxyReader(from)
+
+	_, err = io.CopyN(replica, barReader, realLimit)
+
+	bar.Finish()
+
+	if err != nil && !errors.Is(err, io.EOF) {
+		return err
+	}
 	return nil
+}
+
+func realLimit(offset, limit, fileSize int64) int64 {
+	if limit == 0 || offset+limit > fileSize {
+		return fileSize - offset
+	}
+	return limit
 }
